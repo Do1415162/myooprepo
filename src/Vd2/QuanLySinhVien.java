59830673b7c3f6df88a43d6f528b996b11da734f
admin <admin@MSI>
2024-05-24 08:38:17 +0700

    ví dụ 2

--- src/Vd2/QuanLySinhVien.java
+++ src/Vd2/QuanLySinhVien.java
@@ -1,15 +1,13 @@
 package Vd2;
 
-import Bai03.SinhVien;
-
 public class QuanLySinhVien {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	    SinhVien sv = new SinhVien();
+		SinhVien sv = new SinhVien();
 		sv.hoten = "Ngoc Truong";
-		sv.Namsinh = -2005;
-		sv.SDT = "0967732324";
+		sv.namsinh = -2005;
+		sv.sdt = "0967732324";
 		System.out.println(sv);
 	}
 
