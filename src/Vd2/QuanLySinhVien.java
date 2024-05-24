f6bdf080b53eb4d1eefd4a411be99cb2733425ab
admin <admin@MSI>
2024-05-24 08:33:04 +0700

    vd2

---
+++ src/Vd2/QuanLySinhVien.java
@@ -0,0 +1,16 @@
+package Vd2;
+
+import Bai03.SinhVien;
+
+public class QuanLySinhVien {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    SinhVien sv = new SinhVien();
+		sv.hoten = "Ngoc Truong";
+		sv.Namsinh = -2005;
+		sv.SDT = "0967732324";
+		System.out.println(sv);
+	}
+
+}
