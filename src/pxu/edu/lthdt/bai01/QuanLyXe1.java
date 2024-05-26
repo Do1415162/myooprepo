db707a33e67959e839391e63132c90f81141f174
admin <admin@MSI.lan>
2024-05-26 15:26:11 +0700

    Bai03-Bài tập 1

---
+++ src/pxu/edu/lthdt/bai01/QuanLyXe1.java
@@ -0,0 +1,14 @@
+package pxu.edu.lthdt.bai01;
+
+public class QuanLyXe1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   Oto car = new Oto("Lambotghini", 2023, 800);
+	        QuanLyXeBus bus = new QuanLyXeBus("VinBus", 2023, 400, 30);
+	        
+	        System.out.println(car);
+	        System.out.println(bus);
+	}
+
+}
