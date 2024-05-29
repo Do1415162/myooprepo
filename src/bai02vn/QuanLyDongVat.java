d9a593aa0732eaaec5e2175c314ae2a8a3477ac4
admin <admin@MSI.lan>
2024-05-29 18:43:09 +0700

    bai vd2 nộp muộn

---
+++ src/bai02vn/QuanLyDongVat.java
@@ -0,0 +1,15 @@
+package bai02vn;
+
+public class QuanLyDongVat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		   // Thêm động vật
+        Meo meo1 = new Meo("Meo Miu", 2020, "Cái", 1, "Trắng", "Ba Tư");
+        System.out.println(meo1);
+        
+        Tho tho1 = new Tho("Thỏ Bông", 2021, "Đực", 2, "Nâu", "Bỉ");
+        System.out.println(tho1);
+	}
+
+}
