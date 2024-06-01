93854bc207f123a65354836d8945388ed936ba64
admin <admin@MSI>
2024-06-01 13:40:32 +0700

    bai 5 ví dụ 1 và 2

---
+++ src/bai05/vidu1/Dog.java
@@ -0,0 +1,11 @@
+package bai05.vidu1;
+
+public class Dog extends Animal {
+
+	
+	public void speak() {
+		System.out.println("Woof!");
+	}
+
+ 
+}
---
+++ src/bai05/vidu1/QuanLyDongVat.java
@@ -0,0 +1,13 @@
+package bai05.vidu1;
+
+public class QuanLyDongVat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Animal obj1 = new Animal();
+     obj1.speak();
+     Dog obj2 = new Dog();
+     obj2.speak();
+	}
+
+}
