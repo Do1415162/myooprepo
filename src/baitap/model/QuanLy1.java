66cbd9d8a4ad10231b3ccb71d840f53e0198943c
admin <admin@MSI>
2024-06-22 08:30:18 +0700

    nộp lại lần 3

---
+++ src/baitap/model/QuanLy1.java
@@ -0,0 +1,10 @@
+package baitap.model;
+
+public class QuanLy1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/baitapvn/DiChuyen.java
+++
@@ -1,9 +0,0 @@
-package baitapvn;
-
-public interface DiChuyen {
-	public void trai();
-	public void phai();
-	public void len();
-	public void xuong();
-
-}
